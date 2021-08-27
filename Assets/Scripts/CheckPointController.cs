76a0ef42f14ac5f992e27a201fa6166ebc811da6
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-08-27 11:02:51 -0400

    Checkpoint Logic added

--- Assets/Scripts/CheckPointController.cs
+++ Assets/Scripts/CheckPointController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckPointController : MonoBehaviour
 {
+    public string cpName;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey(SceneManager.GetActiveScene().name + "_cp"))
+        {
+            if (PlayerPrefs.GetString(SceneManager.GetActiveScene().name + "_cp") == cpName)
+            {
+                PlayerController.instance.transform.position = transform.position;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", "");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetString(SceneManager.GetActiveScene().name + "_cp", cpName);
+        }
     }
 }
